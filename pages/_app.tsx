ca5e5ac328529bbfa74d06c7847388e5928d56bf
Nastasya <nastasya@skiff.com>
2023-03-30 16:14:29 +0200

    fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
 import App, { AppProps } from 'next/app';
 import '../styles/globals.css';
+import React from 'react';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element  {
-  return <Component {...pageProps} />;
+  return <>
+    <Head>
+      <title>My top — мой лучший топ</title>
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+			<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&display=swap" rel="stylesheet" />
+    </Head>
+    <Component {...pageProps}/>
+    
+    </>;
 }
 
 export default MyApp;
